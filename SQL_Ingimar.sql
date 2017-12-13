f97d865c8913d50aa0259706be9496edee656f2b
Ingimar <ingimar16@ru.is>
2017-12-13 13:47:27 +0000

    added questions in sql

--- SQL_Ingimar.sql
+++ SQL_Ingimar.sql
@@ -18,3 +18,30 @@ SELECT
 , count(*) FILTER (WHERE question like '%Norway%') as Norway
 , count(*) FILTER (WHERE question like '%Faroe Islands%') as Faroe_Islands
 FROM jeopardy;
+
+--hvaða orð kemur oftast fyrir í svörum með 2 orð
+SELECT answernoun, count(answernoun)
+FROM answernouns
+WHERE answernoun like '% %'
+GROUP BY answernoun
+ORDER BY count(answernoun) DESC;
+
+--hvaða orð kemur oftast fyrir í svörum með 1 orð
+SELECT answernoun, count(answernoun)
+FROM answernouns
+GROUP BY answernoun
+ORDER BY count(answernoun) DESC;
+
+--hvaða orð kemur oftast fyrir í spurningum með 2 orð
+SELECT questionnoun, count(questionnoun)
+FROM questionnouns
+WHERE questionnoun like '% %'
+GROUP BY questionnoun
+ORDER BY count(questionnoun) DESC; 
+
+--hvaða orð kemur oftast fyrir í spurningum með 1 orð
+SELECT questionnoun, count(questionnoun)
+FROM questionnouns
+GROUP BY questionnoun
+ORDER BY count(questionnoun) DESC;
+
