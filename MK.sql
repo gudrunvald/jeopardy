d5b5bf212d65e69d70bfb6103b0bfe87237d373a
maggawaage <maggawaage@gmail.com>
2017-12-13 13:21:36 +0000

    leikarar konur og karlar

--- MK.sql
+++ MK.sql
@@ -4,3 +4,11 @@ from jeopardy
 where question like '%Iceland%')/(SELECT count(question)
 from jeopardy
 where question like '%Denmark%')::float;
+
+#Hversu margir kk og kvk leikarar
+SELECT (select count(gender)
+FROM persons
+WHERE gender like '%male%') as Males,
+(SELECT count(gender)
+from persons
+where gender like '%female%') as Females;
